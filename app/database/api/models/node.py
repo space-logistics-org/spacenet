cd5ad40bb9b82f9edfe672264f09591a8deea87e
Jay Hilton <jhilton@mit.edu>
2021-06-14 00:29:39 -0400

    Fix: use polymorphic identity for node models

--- app/database/api/models/node.py
+++ app/database/api/models/node.py
@@ -1,28 +1,36 @@
 from sqlalchemy import Column, Integer, String, Float
 from ..database import Base
 
+
 class Node(Base):
+    __tablename__ = "Nodes"
 
-    _tablename_ = "Nodes"
-
-    id = Column(Integer, primary_key = True, index = True)
+    id = Column(Integer, primary_key=True, index=True)
     type = Column(String)
     name = Column(String)
     description = Column(String)
     body_1 = Column(String)
 
-class SurfaceNode(Node):
+    __mapper_args__ = {"polymorphic_on": type, "polymorphic_identity": "node"}
 
+
+class SurfaceNode(Node):
     latitude = Column(Float)
     longitude = Column(Float)
 
-class OrbitalNode(Node):
+    __mapper_args__ = {"polymorphic_identity": "surface"}
 
+
+class OrbitalNode(Node):
     apoapsis = Column(Float)
     periapsis = Column(Float)
     inclination = Column(Float)
 
-class LagrangeNode(Node):
+    __mapper_args__ = {"polymorphic_identity": "orbital"}
 
+
+class LagrangeNode(Node):
     body_2 = Column(String)
     lp_number = Column(Integer)
+
+    __mapper_args__ = {"polymorphic_identity": "lagrange"}
