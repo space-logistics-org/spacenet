2fd36f1f0a19666a1a508d5d1372055c3ad4c84e
Jay Hilton <jhilton@mit.edu>
2021-06-14 00:22:55 -0400

    Fix: add polymorphic identity for subclasses
    - Subclasses previously lacked polymorphic identity, which might have
      caused problems

--- app/database/api/models/element.py
+++ app/database/api/models/element.py
@@ -3,7 +3,7 @@ from ..database import Base
 
 
 class Element(Base):
-    _tablename_ = "Elements"
+    __tablename__ = "Elements"
 
     id = Column(Integer, primary_key=True, index=True)
     type = Column(String)
@@ -15,33 +15,48 @@ class Element(Base):
     mass = Column(Float)
     volume = Column(Float)
 
+    __mapper_args__ = {"polymorphic_on": type, "polymorphic_identity": "element"}
+
 
 class CargoCarrier(Element):
+
+    __abstract__ = True
+
     max_cargo_mass = Column(Float)
     max_cargo_volume = Column(Float)
 
 
 class ResourceContainer(CargoCarrier):
-    pass
+
+    __mapper_args__ = {"polymorphic_identity": "resource_container"}
 
 
 class ElementCarrier(CargoCarrier):
     cargo_environment = Column(String)
 
+    __mapper_args__ = {"polymorphic_identity": "element_carrier"}
+
 
 class Agent(Element):
+    __abstract__ = True
+
     active_time_fraction = Column(Float)
 
 
 class HumanAgent(Agent):
+    __mapper_args__ = {"polymorphic_identity": "human_agent"}
     pass
 
 
 class RoboticAgent(Agent):
+    __mapper_args__ = {"polymorphic_identity": "robotic_agent"}
     pass
 
 
 class Vehicle(CargoCarrier):
+
+    __abstract__ = True
+
     max_crew = Column(Integer)
     max_fuel = Column(Float)
 
@@ -50,7 +65,11 @@ class PropulsiveVehicle(Vehicle):
     isp = Column(Float)
     propellant_id = Column(Integer)
 
+    __mapper_args__ = {"polymorphic_identity": "propulsive_vehicle"}
+
 
 class SurfaceVehicle(Vehicle):
     max_speed = Column(Float)
     fuel_id = Column(Integer)
+
+    __mapper_args__ = {"polymorphic_identity": "surface_vehicle"}
