1cad3bb52cdcfbd7be9b247a2603531a090df181
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-06-17 15:18:46 -0400

    Update node.py

--- app/database/api/routers/node.py
+++ app/database/api/routers/node.py
@@ -17,6 +17,18 @@ Nodes = Union[
     LagrangeNode
 ]
 
+UpdateNodes = Union[
+    UpdateSurfaceNode,
+    UpdateOrbitalNode,
+    UpdateLagrangeNode
+]
+
+ReadNodes = Union[
+    ReadSurfaceNode,
+    ReadOrbitalNode,
+    ReadLagrangeNode
+]
+
 SCHEMA_TO_MODEL = {
     SurfaceNode: models.SurfaceNode,
     OrbitalNode: models.OrbitalNode,
@@ -24,7 +36,7 @@ SCHEMA_TO_MODEL = {
 }
 
 
-@router.post("/", response_model=Nodes)
+@router.post("/", response_model=ReadNodes)
 def create_node(
         node: Nodes,
         # token: str = Depends(oauth2_scheme),
@@ -37,7 +49,7 @@ def create_node(
     return db_node
 
 
-@router.get("/", response_model=List[Nodes])
+@router.get("/", response_model=List[ReadNodes])
 def list_nodes(
         skip: int = 0,
         limit: int = 100,
@@ -46,7 +58,7 @@ def list_nodes(
     return db.query(models.Node).offset(skip).limit(limit).all()
 
 
-@router.get("/{node_id}", response_model=Nodes)
+@router.get("/{node_id}", response_model=ReadNodes)
 def read_node(
         node_id: int,
         db: Session = Depends(database.get_db)
@@ -57,7 +69,7 @@ def read_node(
     return db_node
 
 
-@router.put("/{node_id}", response_model=Nodes)
+@router.put("/{node_id}", response_model=ReadNodes)
 def update_node(
         node_id: int,
         node: Nodes,
@@ -75,7 +87,33 @@ def update_node(
     return db_node
 
 
-@router.delete("/{node_id}", response_model=Nodes)
+@router.patch(
+    "/{id_}",
+    response_model=ReadNodes
+)
+def patch_node(
+    id_: int, element: UpdateNodes, db: Session = Depends(database.get_db)
+):
+    db_node = db.query(models.node).get(id_)
+    if db_node is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No node found with id={id_}",
+        )
+    if node.type != db_node.type:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Node found with id={id_} is of type {db_node.type}; cannot update "
+            f"type to {node.type} ",
+        )
+    for field_name, field in node.dict().items():
+        if field_name != "type" and field is not None:
+            setattr(db_node, field_name, field)
+    db.commit()
+    return db_node
+
+
+@router.delete("/{node_id}", response_model=ReadNodes)
 def delete_node(
         node_id: int,
         # token: str = Depends(oauth2_scheme),
