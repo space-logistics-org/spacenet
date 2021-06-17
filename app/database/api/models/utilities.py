4c27dc00d3d86790b2bf10adde596d2ef10ec8ff
Jay Hilton <jhilton@mit.edu>
2021-06-17 00:02:11 -0400

    Add documentation for dictify_row
    - Rename dictify_model to dictify_row

--- app/database/api/models/utilities.py
+++ app/database/api/models/utilities.py
@@ -3,5 +3,11 @@ from typing import Any, Dict
 from app.database.api.database import Base
 
 
-def dictify_model(model: Base) -> Dict[str, Any]:
-    return {col.name: getattr(model, col.name) for col in model.__table__.columns}
+def dictify_row(row: Base) -> Dict[str, Any]:
+    """
+    Convert a database row to a dictionary.
+
+    :param row: row to copy data into dictionary from
+    :return: dictionary mapping column names to values
+    """
+    return {col.name: getattr(row, col.name) for col in row.__table__.columns}
