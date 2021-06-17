313eb9588ed55935c2bb04854a9c7bdc05645fb8
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-06-17 15:17:30 -0400

    Update node.py

--- app/database/api/schemas/node.py
+++ app/database/api/schemas/node.py
@@ -1,6 +1,7 @@
 from spacenet.schemas import node as schemas
 from pydantic import Field
 
+from spacenet.schemas.mixins import ReadSchema, RequiresOnlyType
 
 class SurfaceNodeCreate(schemas.SurfaceNode):
     pass
@@ -45,3 +46,26 @@ class LagrangeNode(schemas.LagrangeNode):
 
     class Config:
         orm_mode = True
+
+class UpdateSurfaceNode(SurfaceNode, RequiresOnlyType):
+    pass
+
+
+class ReadSurfaceNode(SurfaceNode, ReadSchema):
+    pass
+
+
+class UpdateOrbitalNode(OrbitalNode, RequiresOnlyType):
+    pass
+
+
+class ReadOrbitalNode(OrbitalNode, ReadSchema):
+    pass
+
+
+class UpdateLagrangeNode(LagrangeNode, RequiresOnlyType):
+    pass
+
+
+class ReadLagrangeNode(LagrangeNode, ReadSchema):
+    pass
