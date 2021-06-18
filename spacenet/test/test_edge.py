6dfa32ccc94d850396507ac76fd924584300566c
Jay Hilton <jhilton@mit.edu>
2021-06-17 20:34:23 -0400

    Test all bad keyword args for error in edge tests

--- spacenet/test/test_edge.py
+++ spacenet/test/test_edge.py
@@ -7,7 +7,7 @@ from pydantic import ValidationError
 
 from spacenet import test
 from spacenet.schemas import edge as eos
-from spacenet.schemas.edge import FlightEdge, SpaceEdge, SurfaceEdge
+from spacenet.schemas.edge import EdgeType, FlightEdge, SpaceEdge, SurfaceEdge
 
 pytestmark = [pytest.mark.unit, pytest.mark.edge]
 
@@ -70,85 +70,74 @@ class TestEdge(unittest.TestCase):
             badMax_cargoEdge = FlightEdge(**badMax_cargoData)
 
 
-class TestingData(unittest.TestCase):
+class TestFromFile(unittest.TestCase):
+
+    good_edges = json.loads(
+        pkg_resources.resource_string(test.__name__, "good_edges.json")
+    )
+
+    good_surface = list(
+        filter(lambda node: node["type"] == EdgeType.Surface.value, good_edges)
+    )
+    good_space = list(
+        filter(lambda node: node["type"] == EdgeType.Space.value, good_edges)
+    )
+    good_flight = list(
+        filter(lambda node: node["type"] == EdgeType.Flight.value, good_edges)
+    )
+
+    bad_edges = json.loads(
+        pkg_resources.resource_string(test.__name__, "bad_edges.json")
+    )
 
     def test_FlightEdge(self):
 
-        edge_data = json.loads(
-            pkg_resources.resource_string(
-                test.__name__,
-                'good_edges.json'
-            )
-        )
+        for edge in self.good_flight:
 
-        for edge in edge_data:
-
-            if edge["type"] == "Flight":
-                testedge = eos.FlightEdge(**edge)
-                self.assertEqual(testedge.name, edge.get("name"))
-                self.assertEqual(testedge.origin_id, edge.get("origin_id"))
-                self.assertEqual(testedge.destination_id, edge.get("destination_id"))
-                self.assertEqual(testedge.duration, edge.get("duration"))
-                self.assertEqual(testedge.max_crew, edge.get("max_crew"))
-                self.assertEqual(testedge.max_cargo, edge.get("max_cargo"))
-                self.assertEqual(testedge.description, edge.get("description"))
+            testedge = eos.FlightEdge(**edge)
+            self.assertEqual(testedge.name, edge.get("name"))
+            self.assertEqual(testedge.origin_id, edge.get("origin_id"))
+            self.assertEqual(testedge.destination_id, edge.get("destination_id"))
+            self.assertEqual(testedge.duration, edge.get("duration"))
+            self.assertEqual(testedge.max_crew, edge.get("max_crew"))
+            self.assertEqual(testedge.max_cargo, edge.get("max_cargo"))
+            self.assertEqual(testedge.description, edge.get("description"))
 
     def test_SpaceEdge(self):
 
-        edge_data = json.loads(
-            pkg_resources.resource_string(
-                test.__name__,
-                'good_edges.json'
-            )
-        )
+        for edge in self.good_space:
 
-        for edge in edge_data:
-
-            if edge["type"] == "Space":
-                testedge = eos.SpaceEdge(**edge)
-                self.assertEqual(testedge.name, edge.get("name"))
-                self.assertEqual(testedge.origin_id, edge.get("origin_id"))
-                self.assertEqual(testedge.destination_id, edge.get("destination_id"))
-                self.assertEqual(testedge.duration, edge.get("duration"))
-                self.assertEqual(testedge.description, edge.get("description"))
+            testedge = eos.SpaceEdge(**edge)
+            self.assertEqual(testedge.name, edge.get("name"))
+            self.assertEqual(testedge.origin_id, edge.get("origin_id"))
+            self.assertEqual(testedge.destination_id, edge.get("destination_id"))
+            self.assertEqual(testedge.duration, edge.get("duration"))
+            self.assertEqual(testedge.description, edge.get("description"))
 
     def test_SurfaceEdge(self):
 
-        edge_data = json.loads(
-            pkg_resources.resource_string(
-                test.__name__,
-                'good_edges.json'
-            )
-        )
+        for edge in self.good_surface:
 
-        for edge in edge_data:
-
-            if edge["type"] == "Surface":
-                testedge = eos.SurfaceEdge(**edge)
-                self.assertEqual(testedge.name, edge.get("name"))
-                self.assertEqual(testedge.origin_id, edge.get("origin_id"))
-                self.assertEqual(testedge.destination_id, edge.get("destination_id"))
-                self.assertEqual(testedge.distance, edge.get("distance"))
-                self.assertEqual(testedge.description, edge.get("description"))
+            testedge = eos.SurfaceEdge(**edge)
+            self.assertEqual(testedge.name, edge.get("name"))
+            self.assertEqual(testedge.origin_id, edge.get("origin_id"))
+            self.assertEqual(testedge.destination_id, edge.get("destination_id"))
+            self.assertEqual(testedge.distance, edge.get("distance"))
+            self.assertEqual(testedge.description, edge.get("description"))
 
     def test_BadFlightEdge(self):
-        edge = {"type": "Flight", "name": "Test1", "origin_id": 1, "destination_id": 2,
-                "duration": 25, "max_crew": 6, "max_cargo": -100,
-                "description": "Invalid max_cargo input."
-                }
-        with self.assertRaises(ValidationError):
-            bad_edge = eos.FlightEdge(**edge)
+        for edge in self.bad_edges:
+            print(edge)
+            with self.assertRaises(ValidationError):
+                bad_edge = eos.FlightEdge(**edge)
 
     def test_BadSurfaceEdge(self):
-        edge = {"type": "Space", "name": "Test3", "origin_id": 3, "destination_id": 2.5,
-                "duration": 25, "description": "Invalid destination_id input."
-                }
-        with self.assertRaises(ValidationError):
-            bad_edge = eos.SurfaceEdge(**edge)
+        for edge in self.bad_edges:
+            print(edge)
+            with self.assertRaises(ValidationError):
+                bad_edge = eos.SurfaceEdge(**edge)
 
     def test_BadSpaceEdge(self):
-        edge = {"type": "Surface", "name": "Test5", "origin_id": 5, "destination_id": 2,
-                "description": "Invalid distance type."
-                }
-        with self.assertRaises(ValidationError):
-            bad_edge = eos.SpaceEdge(**edge)
+        for edge in self.bad_edges:
+            with self.assertRaises(ValidationError):
+                bad_edge = eos.SpaceEdge(**edge)
