457fd6e76ef909475e6bbee3ebf2ffce8088f8d5
aleph-oh <jhilton@mit.edu>
2021-06-22 02:37:32 -0400

    Constrain element event IDs to prevent coercion
    - Use StrictInt, prevents coercion of floats to ints

--- spacenet/schemas/element_events.py
+++ spacenet/schemas/element_events.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, StrictInt
 
 
 class EntryPointKind(Enum):
@@ -10,12 +10,12 @@ class EntryPointKind(Enum):
 
 
 class MakeElementEvent(BaseModel):
-    element_id: int = Field(
+    element_id: StrictInt = Field(
         ..., description="the id of the element being added to simulation"
     )
     entry_point_kind: EntryPointKind = Field(
         description="the type of entry point the element is entering at"
     )
-    entry_point_id: int = Field(
+    entry_point_id: StrictInt = Field(
         ..., description="the id of the entry point the element is being added at"
     )
