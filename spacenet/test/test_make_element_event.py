59873369e8e1a1d8b00f0db3f09c9cc93e7cdcd6
Jay Hilton <jhilton@mit.edu>
2021-06-22 15:01:47 -0400

    Refactor MakeElement tests to use kw format
    - More in common with other element event tests, so easier to refactor
      later

--- spacenet/test/test_make_element_event.py
+++ spacenet/test/test_make_element_event.py
@@ -6,6 +6,7 @@ from spacenet.test.event_utilities import (
     INVALID_INTS,
     valid_invalid_from_allowed,
     xfail_from_kw,
+    success_from_kw,
 )
 
 pytestmark = [pytest.mark.unit, pytest.mark.event]
@@ -18,43 +19,55 @@ VALID_ENTRY_IDS = st.integers()
 INVALID_ENTRY_IDS = INVALID_ELE_IDS
 
 
-def xfail_construct_make(element_id, kind, entry_id):
+VALID_MAP = {
+    "element_id": st.integers(),
+    "entry_point_kind": VALID_KINDS,
+    "entry_point_id": st.integers(),
+}
+
+INVALID_MAP = {
+    "element_id": INVALID_INTS,
+    "entry_point_kind": INVALID_KINDS,
+    "entry_point_id": INVALID_INTS,
+}
+
+
+def xfail_construct_make(element_id, entry_point_kind, entry_point_id):
     return xfail_from_kw(
         MakeElementEvent,
         element_id=element_id,
-        entry_point_kind=kind,
-        entry_point_id=entry_id,
+        entry_point_kind=entry_point_kind,
+        entry_point_id=entry_point_id,
     )
 
 
+@given(kw=st.fixed_dictionaries(mapping=VALID_MAP),)
+def test_valid(kw):
+    success_from_kw(MakeElementEvent, **kw)
+
+
 @given(
-    ele_id=VALID_ELE_IDS, kind=VALID_KINDS, entry_id=VALID_ENTRY_IDS,
-)
-def test_valid(ele_id, kind, entry_id):
-    event = MakeElementEvent(
-        element_id=ele_id, entry_point_kind=kind, entry_point_id=entry_id
+    kw=st.fixed_dictionaries(
+        mapping={**VALID_MAP, "element_id": INVALID_MAP["element_id"]}
     )
-    assert event.element_id == ele_id
-    assert event.entry_point_kind == kind
-    assert event.entry_point_id == entry_id
+)
+def test_invalid_element_id(kw):
+    xfail_construct_make(**kw)
 
 
 @given(
-    ele_id=INVALID_ELE_IDS, kind=VALID_KINDS, entry_id=VALID_ENTRY_IDS,
+    kw=st.fixed_dictionaries(
+        mapping={**VALID_MAP, "entry_point_kind": INVALID_MAP["entry_point_kind"]}
+    )
 )
-def test_invalid_element_id(ele_id, kind, entry_id):
-    xfail_construct_make(ele_id, kind, entry_id)
+def test_invalid_entry_point_kind(kw):
+    xfail_construct_make(**kw)
 
 
 @given(
-    ele_id=VALID_ELE_IDS, kind=INVALID_KINDS, entry_id=VALID_ENTRY_IDS,
+    kw=st.fixed_dictionaries(
+        mapping={**VALID_MAP, "entry_point_id": INVALID_MAP["entry_point_id"]}
+    )
 )
-def test_invalid_kind(ele_id, kind, entry_id):
-    xfail_construct_make(ele_id, kind, entry_id)
-
-
-@given(
-    ele_id=VALID_ELE_IDS, kind=VALID_KINDS, entry_id=INVALID_ENTRY_IDS,
-)
-def test_invalid_entry_id(ele_id, kind, entry_id):
-    xfail_construct_make(ele_id, kind, entry_id)
+def test_invalid_entry_point_id(kw):
+    xfail_construct_make(**kw)
