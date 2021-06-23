42a99b2f23fc492dea6beac1fb4566ca82ce8d47
aleph-oh <jhilton@mit.edu>
2021-06-23 16:02:50 -0400

    Implement fix, making element events use UUIDs
    - Changed MakeElementEvent to MakeElementsEvent, allowing multiple
      elements to be constructed at once at one point
     - Documented all element events

--- spacenet/test/events/test_make_element_event.py
+++ spacenet/test/events/test_make_element_event.py
@@ -1,26 +1,26 @@
 import pytest
 from hypothesis import given, strategies as st
 
-from spacenet.schemas.element_events import MakeElementEvent
+from spacenet.schemas.element_events import MakeElementsEvent
 from .event_utilities import INVALID_UUIDS, success_from_kw, xfail_from_kw
 
 pytestmark = [pytest.mark.unit, pytest.mark.event]
 
 
 VALID_MAP = {
-    "element_id": st.uuids(),
+    "element_id": st.lists(st.uuids()),
     "entry_point_id": st.uuids(),
 }
 
 INVALID_MAP = {
-    "element_id": INVALID_UUIDS,
+    "element_id": st.lists(INVALID_UUIDS, min_size=1),
     "entry_point_id": INVALID_UUIDS,
 }
 
 
 def xfail_construct_make(element_id, entry_point_id):
     return xfail_from_kw(
-        MakeElementEvent,
+        MakeElementsEvent,
         element_id=element_id,
         entry_point_id=entry_point_id,
     )
@@ -30,7 +30,7 @@ def xfail_construct_make(element_id, entry_point_id):
     kw=st.fixed_dictionaries(mapping=VALID_MAP),
 )
 def test_valid(kw):
-    success_from_kw(MakeElementEvent, **kw)
+    success_from_kw(MakeElementsEvent, **kw)
 
 
 @given(
@@ -38,7 +38,7 @@ def test_valid(kw):
         mapping={**VALID_MAP, "element_id": INVALID_MAP["element_id"]}
     )
 )
-def test_invalid_element_id(kw):
+def test_invalid_element_ids(kw):
     xfail_construct_make(**kw)
 
 
