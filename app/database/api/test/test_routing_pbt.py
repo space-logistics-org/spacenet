898d8c69e526bffbcfc3d023b58f25ca740f44a4
Jay Hilton <jhilton@mit.edu>
2021-06-26 17:43:22 -0400

    Add method bodies for rules

--- app/database/api/test/test_routing_pbt.py
+++ app/database/api/test/test_routing_pbt.py
@@ -18,7 +18,13 @@ from app.database.api import models
 from app.database.api.database import get_db
 from app.database.api.main import app
 from ..schemas import *
-from ..schemas.constants import CREATE_TO_UPDATE
+from ..schemas.constants import (
+    CREATE_TO_UPDATE,
+    EDGE_SCHEMAS,
+    ELEMENT_SCHEMAS,
+    NODE_SCHEMAS,
+    RESOURCE_SCHEMAS,
+)
 from ..models import utilities as model_utils
 from ...test.utilities import test_engine
 
@@ -34,11 +40,18 @@ pytestmark = [
 
 app.dependency_overrid_es[get_db] = get_test_db
 
+TYPE_TO_PREFIX = {
+    **{schema: "/edge" for schema in EDGE_SCHEMAS},
+    **{schema: "/element" for schema in ELEMENT_SCHEMAS},
+    **{schema: "/node" for schema in NODE_SCHEMAS},
+    **{schema: "/resource" for schema in RESOURCE_SCHEMAS},
+}
 
-class TestRouting(RuleBasedStateMachine):
+
+class DatabaseEditorCRUDRoutes(RuleBasedStateMachine):
     def __init__(self):
         super().__init__()
-        self.model = defaultdict(set)
+        self.model = defaultdict(dict)
         self.client = TestClient(app)
         self.clear_tables()
 
@@ -51,31 +64,77 @@ class TestRouting(RuleBasedStateMachine):
     inserted = Bundle("inserted")
 
     def create(self, create_schema):
-        pass
+        prefix = TYPE_TO_PREFIX[type(create_schema)]
+        response = self.client.post(f"{prefix}/", json=create_schema.dict())
+        assert 201 == response.status_code
+        result = response.json()
+        assert dict(create_schema.dict, id=result.get("id")) == result
+        self.model[type(create_schema)][result.get("id")] = result
+        # TODO: what should this return? The entire thing?
 
     def read(self, id_and_type):
-        pass
-    
+        id_, type_ = id_and_type
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.get("/".join((prefix, id_)))
+        assert 200 == response.status_code
+        assert id_ in self.model[type_]
+        assert self.model[type_][id_] == response.json()
+
     def read_invalid_id(self, id_and_type):
-        pass
-    
+        id_, type_ = id_and_type
+        assume(id_ not in self.model[type_])
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.get("/".join((prefix, id_)))
+        assert 404 == response.status_code
+
     def read_all(self):
-        pass
+        for type_, entries in self.model.items():
+            prefix = TYPE_TO_PREFIX[type_]
+            response = self.client.get(f"{prefix}/")
+            assert 200 == response.status_code
+            result = response.json()
+            assert len(entries) == len(result)
+            for entry in result:
+                id_ = entry.get("id")
+                assert id_ in entries
+                assert entries[id_] == entry
 
     def update(self, id_type_and_schema):
-        pass
+        id_, type_, update_schema = id_type_and_schema
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.patch("/".join((prefix, id_)), json=update_schema.dict())
+        assert 200 == response.status_code
+        # TODO: if storing entire objects on bundle, this has to consume
 
-    def update_invalid_id(self, id_and_type):
-        pass
+    def update_invalid_id(self, id_type_and_schema):
+        id_, type_, update_schema = id_type_and_schema
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.patch("/".join((prefix, id_)), json=update_schema.dict())
+        assert 404 == response.status_code
 
     def update_type_mismatch(self, id_type_and_schema):
-        pass
+        id_, type_, update_schema = id_type_and_schema
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.patch("/".join((prefix, id_)), json=update_schema.dict())
+        assert 409 == response.status_code
 
     def delete(self, id_and_type):
-        pass
+        id_, type_ = id_and_type
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.delete("/".join((prefix, id_)))
+        assert 200 == response.status_code
+        result = response.json()
+        assert self.model[type_].pop(result.get("id")) == result
 
     def delete_invalid_id(self, id_and_type):
-        pass
+        id_, type_ = id_and_type
+        assume(id_ not in self.model[type_])
+        prefix = TYPE_TO_PREFIX[type_]
+        response = self.client.delete("/".join((prefix, id_)))
+        assert 404 == response.status_code
 
     def teardown(self):
         self.clear_tables()
+
+
+TestRouting = DatabaseEditorCRUDRoutes.TestCase
