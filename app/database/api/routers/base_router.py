0c2db4cbc2f4e7277920110e9c951e66bed9c12d
Jay Hilton <jhilton@mit.edu>
2021-06-27 17:17:00 -0400

    Make CRUDRouter class provide summar and desc.

--- app/database/api/routers/base_router.py
+++ app/database/api/routers/base_router.py
@@ -8,13 +8,13 @@ NOT_FOUND_RESPONSE = {status.HTTP_404_NOT_FOUND: {"msg": str}}
 
 class CRUDRouter(APIRouter):
     def __init__(
-            self,
-            table,
-            name_lower,
-            name_capitalized,
-            create_schema,
-            read_schema,
-            update_schema,
+        self,
+        table,
+        name_lower,
+        name_capitalized,
+        create_schema,
+        read_schema,
+        update_schema,
     ):
         super().__init__()
         self.add_api_route(
@@ -22,32 +22,42 @@ class CRUDRouter(APIRouter):
             list_all(table),
             methods=["GET"],
             response_model=List[read_schema],
+            summary=f"List {name_capitalized}s",
+            description=f"List {name_lower}s currently in the database.",
         )
         self.add_api_route(
             "/{item_id}",
             read_item(table, name_lower),
             methods=["GET"],
             response_model=read_schema,
-            responses=NOT_FOUND_RESPONSE
+            responses=NOT_FOUND_RESPONSE,
+            summary=f"Read {name_capitalized}",
+            description=f"Find a specific {name_lower} in the database.",
         )
         self.add_api_route(
             "/",
             create_item(create_schema),
             methods=["POST"],
             response_model=read_schema,
-            status_code=status.HTTP_201_CREATED
+            status_code=status.HTTP_201_CREATED,
+            summary=f"Create {name_capitalized}",
+            description=f"Add a new {name_lower} to the database.",
         )
         self.add_api_route(
             "/{item_id}",
             update_item(table, name_capitalized, update_schema),
             methods=["PATCH"],
             response_model=read_schema,
-            responses={**NOT_FOUND_RESPONSE, status.HTTP_409_CONFLICT: {"msg": str}}
+            responses={**NOT_FOUND_RESPONSE, status.HTTP_409_CONFLICT: {"msg": str}},
+            summary=f"Update {name_capitalized}",
+            description=f"Update an existing {name_lower} in the database.",
         )
         self.add_api_route(
             "/{item_id}",
             delete_item(table, name_lower),
             methods=["DELETE"],
             response_model=read_schema,
-            responses=NOT_FOUND_RESPONSE
+            responses=NOT_FOUND_RESPONSE,
+            summary=f"Delete {name_capitalized}",
+            description=f"Delete an existing {name_lower} from the database.",
         )
