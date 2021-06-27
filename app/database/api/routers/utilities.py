ac60c48fba4280d4de6d5d814203934dc67b5ac5
Jay Hilton <jhilton@mit.edu>
2021-06-27 15:30:34 -0400

    Add router with default CRUD routes

--- app/database/api/routers/utilities.py
+++ app/database/api/routers/utilities.py
@@ -1,15 +1,18 @@
 import doctest
 from functools import reduce
-from typing import Iterable, Set, Tuple, Union
+from typing import Iterable, Set, Tuple, TypeVar, Union
 
+from pydantic.main import Model
 from typing_extensions import Type
 
 from ..schemas.constants import CREATE_SCHEMAS, READ_SCHEMAS, UPDATE_SCHEMAS
 
 __all__ = ["union_from_iterable", "create_read_update_unions"]
 
+T = TypeVar("T")
 
-def union_from_iterable(it: Iterable[Type]) -> Union[Type]:
+
+def union_from_iterable(it: Iterable[Type[T]]) -> Union[Type[T]]:
     """
     Provide a union over all of the types provided in the iterable "it".
 
@@ -38,8 +41,8 @@ def union_from_iterable(it: Iterable[Type]) -> Union[Type]:
 
 
 def create_read_update_unions(
-    schema_variants: Set[Type],
-) -> Tuple[Union[Type], Union[Type], Union[Type]]:
+        schema_variants: Set[Type],
+) -> Tuple[Union[Type[Model]], Union[Type[Model]], Union[Type[Model]]]:
     return (
         union_from_iterable(schema_variants & CREATE_SCHEMAS),
         union_from_iterable(schema_variants & READ_SCHEMAS),
