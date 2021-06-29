06fbc6334ebfd0c4ba1901fd7653046b0a9cc0cb
Jay Hilton <jhilton@mit.edu>
2021-06-29 16:34:13 -0400

    Update State model to require element id
    - Cleaned up imports

--- app/database/api/models/state.py
+++ app/database/api/models/state.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 from app.database.api.database import Base
-from .element import Element
 
 __all__ = [
     "State"
@@ -13,7 +12,7 @@ class State(Base):
     __tablename__ = "State"
 
     id = Column(Integer, primary_key=True, index=True)
-    element_id = Column(Integer, ForeignKey("Elements.id", ondelete="CASCADE"))
+    element_id = Column(Integer, ForeignKey("Elements.id", ondelete="CASCADE"), nullable=False)
     name = Column(String)
     state_type = Column(String)
     is_initial_state = Column(Boolean)
