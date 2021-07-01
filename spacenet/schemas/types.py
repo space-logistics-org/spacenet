b0db4f73a261313e0be439f86d8af11daea95a22
Jay Hilton <jhilton@mit.edu>
2021-07-01 13:05:32 -0400

    Add file containing constrained numeric types

---
+++ spacenet/schemas/types.py
@@ -0,0 +1,21 @@
+from math import inf
+
+from pydantic import confloat, conint
+
+from spacenet.constants import SQLITE_MAX_INT, SQLITE_MIN_INT
+
+__all__ = [
+    "SafeInt",
+    "SafePosInt",
+    "SafeNonNegInt",
+    "SafeFloat",
+    "SafePosFloat",
+    "SafeNonNegFloat"
+]
+
+SafeInt = conint(ge=SQLITE_MIN_INT, le=SQLITE_MAX_INT, strict=True)
+SafePosInt = conint(gt=0, le=SQLITE_MAX_INT, strict=True)
+SafeNonNegInt = conint(ge=0, le=SQLITE_MAX_INT, strict=True)
+SafeFloat = confloat(gt=-inf, lt=inf)
+SafePosFloat = confloat(gt=0, lt=inf)
+SafeNonNegFloat = confloat(ge=0, lt=inf)
