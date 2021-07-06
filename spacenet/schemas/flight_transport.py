af16d9fc169f8db405970e61a0a858c1c6e5c9f4
RyanSav93 <43710709+RyanSav93@users.noreply.github.com>
2021-07-06 15:24:34 -0400

    Added transport event schemas
    Added event schemas for flight transport and surface transport

---
+++ spacenet/schemas/flight_transport.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, PositiveInt, PositiveFloat
+
+
+
+class FlightTransport(BaseModel):
+
+    # Schema for Flight Transport
+
+    name: str = Field(..., title = "Name", description = "The flight transport name")
+
+    node: str = Field(..., title = "Node", description = "The origin of the flight transport")
+
+    time: PositiveFloat = Field(..., title = "Time", description = "The execution time")
+
+    priority: PositiveInt = Field(..., title = "Priority", description = "The importance of the mission event", ge = 1, le = 5)
+
+    trajectory: str = Field(..., title = "Trajectory", description = "The nodes that the vehicle will travel to and from")
+
+    elements: str = Field(..., title = "Elements", description = "The elements")
