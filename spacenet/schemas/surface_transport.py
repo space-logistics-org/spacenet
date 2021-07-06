4d54befbe372fce434102632aa1f464e2074ebf4
RyanSav93 <43710709+RyanSav93@users.noreply.github.com>
2021-07-06 15:52:30 -0400

    Fixed the priority field
    PositiveInt takes precedence over constraints, causing constraints to be ignored

--- spacenet/schemas/surface_transport.py
+++ spacenet/schemas/surface_transport.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, PositiveInt, PositiveFloat
+from pydantic import BaseModel, Field, PositiveFloat
 
 
 
@@ -12,7 +12,7 @@ class SurfaceTransport(BaseModel):
 
     time: PositiveFloat = Field(..., title = "Time", description = "The execution time")
 
-    priority: PositiveInt = Field(..., title = "Priority", description = "The importance of the mission event", ge = 1, le = 5)
+    priority: int = Field(..., title = "Priority", description = "The importance of the mission event", ge = 1, le = 5)
 
     trajectory: str = Field(..., title = "Trajectory", description = "The nodes that the vehicle will travel to and from")
 
