bd6b46f1d6e34884141b9204ab42d0d2978eaaac
Leigha Capra <13626046+leighacapra@users.noreply.github.com>
2021-07-08 22:53:44 -0400

    added files for additional formviews

--- app/campaign/static/main_menu.js
+++ app/campaign/static/main_menu.js
@@ -2,6 +2,7 @@ function statusSet() {
   $("#elDirect").hide();
   $("#missionDirect").hide();
   $("#resDirect").hide();
+  $("#visDirect").hide();
 }
 
 
@@ -10,19 +11,32 @@ function elDiv(){
   $("#elDirect").show();
   $("#missionDirect").hide();
   $("#resDirect").hide();
+  $("#visDirect").hide();
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="create_elements.html" ></object>';
 }
 
 function misDiv(){
   $("#elDirect").hide();
   $("#missionDirect").show();
   $("#resDirect").hide();
+  $("#visDirect").hide();
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="crewed_exploration_formview.html" ></object>';
 }
 
 function resDiv(){
   $("#elDirect").hide();
   $("#missionDirect").hide();
   $("#resDirect").show();
+  $("#visDirect").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="consume_resources_formview.html" ></object>';
+}
+
+function visDiv(){
+  $("#elDirect").hide();
+  $("#missionDirect").hide();
+  $("#resDirect").hide();
+  $("#visDirect").show();
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="bat_chart.html" ></object>';
 }
 
 
@@ -44,4 +58,20 @@ function consumeDiv(){
 
 function transferDiv(){
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="transfer_resources_formview.html" ></object>';
+}
+
+function batDiv(){
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="bat_chart.html" ></object>';
+}
+
+function netDiv(){
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="network_visualization.html" ></object>';
+}
+
+function crewDiv(){
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="crewed_exploration_formview.html" ></object>';
+}
+
+function sptrDiv(){
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="spaceTransport_formview.html" ></object>';
 }
--- app/database/static/edge_formview.js
+++ app/database/static/edge_formview.js
@@ -32,125 +32,111 @@ function formSet(modalType) {
   console.log(edgeType)
 
   switch(edgeType) {
+
     case 'def':{
-      $("#" + modalType + "InputName").hide();
-      $("#" + modalType + "InputNameLabel").hide();
-      $("#" + modalType + "InputOrigin_ID").hide();
-      $("#" + modalType + "InputOrigin_IDLabel").hide();
-      $("#" + modalType + "InputDestination_ID").hide();
-      $("#" + modalType + "InputDestination_IDLabel").hide();
-      $("#" + modalType + "InputDuration").hide();
-      $("#" + modalType + "InputDurationLabel").hide();
-      $("#" + modalType + "InputDistance").hide();
-      $("#" + modalType + "InputDistanceLabel").hide();
-      $("#" + modalType + "InputMax_Crew").hide();
-      $("#" + modalType + "InputMax_CrewLabel").hide();
-      $("#" + modalType + "InputMax_Cargo").hide();
-      $("#" + modalType + "InputMax_CargoLabel").hide();
-      $("#" + modalType + "InputDescription").hide();
-      $("#" + modalType + "InputDescriptionLabel").hide();
-      break;
-    }
-    case 'FlightEdge':
-    case 'Flight':
-       {
-      $("#" + modalType + "InputName").show();
-      $("#" + modalType + "InputNameLabel").show();
-
-      $("#" + modalType + "InputOrigin_ID").show();
-      $("#" + modalType + "InputOrigin_IDLabel").show();
-
-      $("#" + modalType + "InputDestination_ID").show();
-      $("#" + modalType + "InputDestination_IDLabel").show();
-
-      $("#" + modalType + "InputDuration").show();
-      $("#" + modalType + "InputDurationLabel").show();
-
-      $("#" + modalType + "InputMax_Crew").show();
-      $("#" + modalType + "InputMax_CrewLabel").show();
-
-      $("#" + modalType + "InputMax_Cargo").show();
-      $("#" + modalType + "InputMax_CargoLabel").show();
-
-      $("#" + modalType + "InputDescription").show();
-      $("#" + modalType + "InputDescriptionLabel").show();
-
-      break;
-        }
-    case 'SpaceEdge':
-    case 'Space':
-      {
-      $("#" + modalType + "InputName").show();
-      $("#" + modalType + "InputNameLabel").show();
-
-      $("#" + modalType + "InputOrigin_ID").show();
-      $("#" + modalType + "InputOrigin_IDLabel").show();
-
-      $("#" + modalType + "InputDestination_ID").show();
-      $("#" + modalType + "InputDestination_IDLabel").show();
-
-      $("#" + modalType + "InputDuration").show();
-      $("#" + modalType + "InputDurationLabel").show();
-
-      $("#" + modalType + "InputDescription").show();
-      $("#" + modalType + "InputDescriptionLabel").show();
-
+      $("#components #inputName").show();
+      $("#components #inputOrigin_ID").show();
+      $("#components #inputDestination_ID").show();
+      $("#compo cxnents #inputDuration").show();
+      $("#components #inputDistance").show();
+      $("#components #inputMax_Crew").show();
+      $("#components #inputMax_Cargo").show();
+      $("#components #inputDescription").show();
       break;
     }
 
-    case 'SurfaceEdge':
-    case 'Surface':
-      {
-      $("#" + modalType + "InputName").show();
-      $("#" + modalType + "InputNameLabel").show();
+    case 'Flight': {
+      $(".name").css("display", "block");
+      $(".ori").css("display", "block");
+      $(".desti").css("display", "block");
+      $(".dura").css("display", "block");
+      $(".crew").css("display", "block");
+      $(".cargo").css("display", "block");
+      $(".descr").css("display", "block");
 
-      $("#" + modalType + "InputOrigin_ID").show();
-      $("#" + modalType + "InputOrigin_IDLabel").show();
+      $(".dista").css({display: "none"});
+      break;
+    }
 
-      $("#" + modalType + "InputDestination_ID").show();
-      $("#" + modalType + "InputDestination_IDLabel").show();
+    case 'Space':{
+      $(".name").css("display", "initial");
+      $(".ori").css("display", "initial");
+      $(".desti").css("display", "initial");
+      $(".dura").css("display", "initial");
+      $(".descr").css("display", "initial");
 
-      $("#" + modalType + "InputDistance").show();
-      $("#" + modalType + "InputDistanceLabel").show();
+      $(".dista").css({display: "none"});
+      $(".crew").css({display: "none"});
+      $(".cargo").css({display: "none"});
+      break;
+    }
 
-      $("#" + modalType + "InputDescription").show();
-      $("#" + modalType + "InputDescriptionLabel").show();
-      break;}
+    case 'Surface':{
+      $(".name").css("display", "initial");
+      $(".ori").css("display", "initial");
+      $(".desti").css("display", "initial");
+      $(".dista").css("display", "initial");
+      $(".descr").css("display", "initial");
+
+      $(".dura").css({display: "none"});
+      $(".crew").css({display: "none"});
+      $(".cargo").css({display: "none"});
+      break;
+    }
   }
 }
 
+function onComplete(){
 
-function getMessage(modalType) {
+    name = document.getElementById("inputName").value;
+    type = document.getElementById("dropPick").value;
+    origin_id = document.getElementById("inputOrigin_ID").value;
+    dest_id = document.getElementById("inputDestination_ID").value;
+    dur = document.getElementById("inputDuration").value;
+    dist = document.getElementById("inputDistance").value;
+    max_crew = document.getElementById("inputMax_Crew").value;
+    max_cargo = document.getElementById("inputMax_Cargo").value;
+    desc = document.getElementById("inputDescription").value;
 
-  name = $("#" + modalType + "InputName").val();
-  type = $("#" + modalType + "DropPick").val();
-  origin_id = $("#" + modalType + "InputOrigin_ID").val();
-  dest_id = $("#" + modalType + "InputDestination_ID").val();
-  dur = $("#" + modalType + "InputDuration").val();
-  dist = $("#" + modalType + "InputDistance").val();
-  max_crew = $("#" + modalType + "InputMax_Crew").val();
-  max_cargo = $("#" + modalType + "InputMax_Cargo").val();
-  desc = $("#" + modalType + "InputDescription").val();
+    switch(type){
 
-  switch(type){
-
-    case "FlightEdge":
-    case 'Flight':
-      {
+      case "Flight":{
+          message = JSON.stringify({
+          type : "Flight",
+          name : name,
+          origin_id :  parseInt(origin_id),
+          destination_id : parseInt(dest_id),
+          duration : parseInt(dur),
+          max_crew : parseInt(max_crew),
+          max_cargo : parseInt(max_cargo),
+          description : desc,
+          });
+          break;
+        }
+      case "Space":{
         message = JSON.stringify({
-        type : "FlightEdge",
-        name : name,
-        origin_id :  parseInt(origin_id),
-        destination_id : parseInt(dest_id),
-        duration : parseInt(dur),
-        max_crew : parseInt(max_crew),
-        max_cargo : parseInt(max_cargo),
-        description : desc,
+          type : "Space",
+          name : name,
+          origin_id :  parseInt(origin_id),
+          destination_id : parseInt(dest_id),
+          duration : parseInt(dur),
+          description : desc,
+      });
+      break;
+    }
+      case "Surface":{
+        message = JSON.stringify({
+          type : "Surface",
+          name : name,
+          origin_id :  parseInt(origin_id),
+          destination_id : parseInt(dest_id),
+          distance : parseInt(dist),
+          description : desc,
         });
         break;
       }
     case "SpaceEdge":
-    case 'Space': 
+    case 'Space':
     {
       message = JSON.stringify({
         type : "SpaceEdge",
@@ -177,34 +163,19 @@ function getMessage(modalType) {
   }
 }
 
-return message;
+  console.log(message)
+  $.ajax({
+    url: "/database/api/edge/",
+    data: message,
+    contentType: 'application/json; charset=utf-8',
+    dataType: "json",
+    method: "POST",
+    success: function() {
+      document.getElementById("edge").reset()
+      document.getElementById("components").reset()
+      location.href = 'edge_table.html'
+    }
+  });
+
+
 }
-
-
-
-function formFill(data) {
-  console.log(data)
-  var edgeType = data.type
-  console.log(edgeType)
-  $('#editDropPick').val(edgeType).trigger('change')
-  $("#editInputName").val(data.name)
-  $("#editInputOrigin_ID").val(data.origin_id)
-  $("#editInputDestination_ID").val(data.destination_id)
-  $("#editInputDescription").val(data.description)
-
-  if (edgeType === 'Flight') 
-    {
-      $("#editInputMax_Crew").val(data.max_crew);
-      $("#editInputMax_Cargo").val(data.max_cargo);
-      $("#editInputDuration").val(data.duration);
-
-    }
-  else if (edgeType === 'Space')
-    {
-      $("#editInputDuration").val(data.duration);
-    }
-  else if (edgeType === 'Surface')
-    {
-      $("#editInputDistance").val(data.distance);
-    }
-  }
