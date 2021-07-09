8c3f1e24906b550e1a34b1db74d153bfdce97d9d
joedubs24 <61467387+joedubs24@users.noreply.github.com>
2021-07-09 12:47:49 -0400

    Schema Addition
    Adding assigned schemas

---
+++ spacenet/schemas/consume-resources.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from typing import Dict, List
+from .types import SafeInt
+from pydantic import BaseModel, Field
+from .resource import Resource 
+class ConsumeRescource(BaseModel):
+    removal_point_id : SafeInt = Field(..., title="Location ID", description = "ID of the node or edge to remove resources from")
+    to_remove: Resource = Field(..., title="Consumed Resource", description = "resource to remove")
+    quantity: float = Field(..., title="Consumed Quantity", description = "quantity to remove")
