fbb7bfe47e3636868eb5d4693e6509d7038e6523
Jay Hilton <jhilton@mit.edu>
2021-07-18 13:26:04 -0400

    Update min heap to support indexing, improve tests

--- spacenet/analysis/min_heap.py
+++ spacenet/analysis/min_heap.py
@@ -1,18 +1,21 @@
 from heapq import heapify, heappop, heappush
-from typing import Generic, List, Optional, TypeVar
+from typing import Generic, Iterable, Optional, TypeVar
 
 T = TypeVar("T")
 
 
 class MinHeap(Generic[T]):
-
+    """
+    A wrapper around the procedural methods provided in the heapq library. Provides a minimal
+    priority queue implementation via an array-backed binary heap.
+    """
     __slots__ = ["_inner"]
 
-    def __init__(self, initial: Optional[List[T]] = None):
+    def __init__(self, initial: Optional[Iterable[T]] = None):
         if initial is None:
             self._inner = []
         else:
-            self._inner = initial.copy()
+            self._inner = list(initial)
             heapify(self._inner)
 
     def peek(self) -> Optional[T]:
@@ -28,5 +31,11 @@ class MinHeap(Generic[T]):
             return None
         return heappop(self._inner)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._inner)
+
+    def __getitem__(self, index: int) -> T:
+        return self._inner[index]
+
+    def __repr__(self) -> str:
+        return repr(f'MinHeap({self._inner})')
--- spacenet/analysis/test/test_min_heap.py
+++ spacenet/analysis/test/test_min_heap.py
@@ -1,12 +1,23 @@
 from heapq import heapify, heappop, heappush
+from itertools import tee
+from typing import Callable, Iterable, List, Tuple, TypeVar
 
 import pytest
-from hypothesis import strategies as st
-from hypothesis.stateful import RuleBasedStateMachine, rule
+from hypothesis import given, strategies as st
+from hypothesis.stateful import RuleBasedStateMachine, invariant, rule
 
 from spacenet.analysis.min_heap import MinHeap
 
 pytestmark = [pytest.mark.analysis]
+
+
+def check_heap_invariant(heap: MinHeap) -> None:
+    n = len(heap)
+    for i in range(n):
+        if 2 * i + 1 < n:
+            assert heap[i] <= heap[2 * i + 1]
+        if 2 * i + 2 < n:
+            assert heap[i] <= heap[2 * i + 2]
 
 
 class HeapModel(RuleBasedStateMachine):
@@ -35,18 +46,71 @@ class HeapModel(RuleBasedStateMachine):
         item = heappop(self.model)
         assert item == self.heap.pop()
 
-    @rule()
+    @invariant()
     def same_len(self):
         assert len(self.model) == len(self.heap)
 
-    @rule()
-    def peek(self):
+    @invariant()
+    def heap_invariant(self):
+        check_heap_invariant(self.heap)
+
+    @invariant()
+    def correct_min(self):
         if not self.model:
             assert 0 == len(self.heap)
             assert not self.heap
             return
         item = self.model[0]
-        assert item == self.heap.peek()
+        assert item == self.heap.peek() == self.heap[0]
 
 
-TestMinHeap = HeapModel.TestCase
+TestMinHeapStateful = HeapModel.TestCase
+
+T = TypeVar("T")
+DrawFn = Callable[[st.SearchStrategy[T]], T]
+
+
+@st.composite
+def iterable_and_list(
+    draw: DrawFn, elements: st.SearchStrategy[T]
+) -> Tuple[Iterable[T], List[T]]:
+    xs = draw(st.iterables(elements))
+    xs, xs_cpy = tee(xs)
+    xs_cpy = list(xs_cpy)
+    return xs, xs_cpy
+
+
+@given(xs_and_copy=iterable_and_list(st.integers()))
+def test_min_heap(xs_and_copy):
+    xs, xs_cpy = xs_and_copy
+    from_init = MinHeap(xs)
+    check_heap_invariant(from_init)
+    via_push = MinHeap()
+    for n in xs_cpy:
+        via_push.push(n)
+    check_heap_invariant(via_push)
+    assert from_init.peek() == via_push.peek()
+    if not xs_cpy:
+        with pytest.raises(IndexError):
+            _should_fail = from_init[0]
+        with pytest.raises(IndexError):
+            _should_fail = via_push[0]
+        assert from_init.peek() is None
+        assert via_push.peek() is None
+
+
+@given(
+    xs_and_copy=st.one_of(
+        *(
+            iterable_and_list(t)
+            for t in (st.integers(), st.floats(allow_nan=False), st.text())
+        )
+    )
+)
+def test_infallible_push(xs_and_copy):
+    xs, xs_cpy = xs_and_copy
+    heap = MinHeap()
+    for n in xs:
+        heap.push(n)
+    if xs_cpy:
+        assert min(xs_cpy) == heap.peek()
