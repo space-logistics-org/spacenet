e429d7a91aeeae8adc3f92435983913ceb1e98ed
Jay Hilton <jhilton@mit.edu>
2021-07-20 18:11:21 -0400

    Clean up dependencies and main

--- app/database/api/test/test_resource_pbt.py
+++ app/database/api/test/test_resource_pbt.py
@@ -10,7 +10,7 @@ from app.database.api.database import get_db
 from app.database.api.main import app
 from app.database.api.models.resource import Resource as ResourceModel
 from app.database.test.utilities import test_engine
-from app.dependencies import current_user
+from app.auth_dependencies import current_user
 from spacenet.constants import SQLITE_MAX_INT, SQLITE_MIN_INT
 from .utilities import get_current_user, get_test_db
 from ..schemas.constants import CREATE_TO_UPDATE
--- app/main.py
+++ app/main.py
@@ -1,69 +1,72 @@
-import databases
-import sqlalchemy
-from fastapi import FastAPI, Request, Depends
+from fastapi import Depends, FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi_users import FastAPIUsers, models
-from fastapi_users.authentication import JWTAuthentication, CookieAuthentication
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from fastapi_users.user import UserAlreadyExists
+from pydantic import EmailStr
 
-from typing import Optional
-from pydantic import BaseModel
-from .dependencies import fastapi_users, jwt_authentication, cookie_authentication, on_after_register, User, UserCreate, database
+from .database import main as database_app
+from .campaign import main as campaign
+
+from .auth_dependencies import (
+    User,
+    UserCreate,
+    cookie_authentication,
+    database,
+    fastapi_users,
+    jwt_authentication,
+)
 
 # define the application
 app = FastAPI(
     title="SpaceNet",
     description="Modeling and simulation for space exploration campaign logistics.",
-    version="3.0.0"
+    version="3.0.0",
 )
 
 # mount constituent applications
-from .database import main as database_app
-app.mount('/database', database_app.app)
 
-from .campaign import main as campaign
-app.mount('/campaign', campaign.app)
 
+app.mount("/database", database_app.app)
+
+app.mount("/campaign", campaign.app)
 
 app.include_router(
     fastapi_users.get_auth_router(jwt_authentication), prefix="/auth/jwt", tags=["auth"]
 )
 app.include_router(
-    fastapi_users.get_auth_router(cookie_authentication), prefix="/auth/cookie", tags=["auth"]
+    fastapi_users.get_auth_router(cookie_authentication),
+    prefix="/auth/cookie",
+    tags=["auth"],
 )
 
 app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
 
+
 @app.get("/secret")
-async def secret(user: User = Depends(fastapi_users.current_user(active=True))):
-    return {
-        "secret": "secret"
-    }
+async def secret(_user: User = Depends(fastapi_users.current_user(active=True))):
+    return {"secret": "secret"}
+
 
 @app.get("/super-secret")
-async def super_secret(user: User = Depends(fastapi_users.current_user(active=True,superuser=True))):
-    return {
-        "secret": "secret"
-    }
+async def super_secret(
+        _user: User = Depends(fastapi_users.current_user(active=True, superuser=True))
+):
+    return {"secret": "secret"}
+
 
 # mount the static directory
 app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
+
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
     try:
         await fastapi_users.create_user(
-            UserCreate(
-                email="admin@example.com",
-                password="admin",
-                is_superuser=True,
-            )
+            UserCreate(email=EmailStr("admin@example.com"), password="admin",
+                       is_superuser=True, )
         )
-    except:
-        print(f'Admin account already exists, skipping.')
-
+    except UserAlreadyExists:
+        print(f"Admin account already exists, skipping.")
 
 
 @app.on_event("shutdown")
