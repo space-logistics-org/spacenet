7e687c89c4f2a9de55694a8d0027fcb4b4605b7f
Jay Hilton <jhilton@mit.edu>
2021-07-21 00:21:36 -0400

    Reformat mission and consume_resources

--- spacenet/schemas/mission.py
+++ spacenet/schemas/mission.py
@@ -14,11 +14,15 @@ from .mission_demand_model import MissionDemand
 
 
 class Mission(BaseModel):
-    name: str = Field(..., title = "Name", description = "name of mission")
-    start_date: datetime = Field(..., title = "Start Date", description = "date of mission start")
-    events: List[SafeInt] = Field(..., title = "Event ID List", description = "list of event IDs")
-    demand_models : List[MissionDemand] = Field(..., title = "Demand Models List", description = "list of mission demand models")
-    origin: SafeInt = Field(..., title = "Origin ID", description = "ID of origin node")
-    destination: SafeInt = Field(..., title="Destination ID", description = "ID of destination node")
-    return_origin: SafeInt = Field(..., title = "Return Origin ID", description = "ID of return origin node")
-    return_destination: SafeInt = Field(..., title = "Return Destination ID", description = "ID of return destination node")
+    name: str = Field(..., title="Name", description="name of mission")
+    start_date: datetime = Field(..., title="Start Date", description="date of mission start")
+    events: List[SafeInt] = Field(..., title="Event ID List", description="list of event IDs")
+    demand_models: List[MissionDemand] = Field(..., title="Demand Models List",
+                                               description="list of mission demand models")
+    origin: SafeInt = Field(..., title="Origin ID", description="ID of origin node")
+    destination: SafeInt = Field(..., title="Destination ID",
+                                 description="ID of destination node")
+    return_origin: SafeInt = Field(..., title="Return Origin ID",
+                                   description="ID of return origin node")
+    return_destination: SafeInt = Field(..., title="Return Destination ID",
+                                        description="ID of return destination node")
