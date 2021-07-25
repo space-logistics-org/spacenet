11268841293e5b393abcccdb6b75af1fe68bd569
Jay Hilton <jhilton@mit.edu>
2021-07-25 01:38:20 -0400

    Make spatial simulation tests fail for valid reason
    - Previously failed because of inability to JSON-encode

--- app/campaign/api/test/test_spatial_simulation.py
+++ app/campaign/api/test/test_spatial_simulation.py
@@ -1,6 +1,5 @@
-import json
-
 import pytest
+from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 from hypothesis import given, strategies as st
 
@@ -14,24 +13,33 @@ pytestmark = [pytest.mark.integration, pytest.mark.analysis]
 client = TestClient(app)
 
 
-@given(scenario=st.builds(Scenario))
 @pytest.mark.slow
 @pytest.mark.xfail
-def test_only_allowed_error_codes(scenario: Scenario):
-    response = client.get("/simulation", json=json.loads(scenario.json()))
+@given(scenario=st.builds(Scenario))
+def test_only_allowed_status_codes(scenario: Scenario):
+    response = client.get("/simulation", json=jsonable_encoder(scenario.dict()))
     assert 200 == response.status_code
     # for now 200 is the only acceptable status code;
     # may allow different status code for error case
 
 
-@given(scenario=st.builds(Scenario))
+def error_case(response_json, sim_errors):
+    assert response_json == [error.json() for error in sim_errors]
+
+
+def success_case(response_json, sim):
+    assert SimResult.parse_obj(response_json) == SimResult.from_sim(sim), response_json
+
+
 @pytest.mark.slow
 @pytest.mark.xfail
+@given(scenario=st.builds(Scenario))
 def test_same_result_as_analysis(scenario: Scenario):
-    response = client.get("/simulation", json=json.loads(scenario.json()))
+    response = client.get("/simulation", json=jsonable_encoder(scenario.dict()))
+    response_json = response.json()
     sim = Simulation(scenario)
     expected_errors = sim.run()
     if expected_errors:
-        assert response.json() == [error.json() for error in expected_errors]
+        error_case(response_json, expected_errors)
     else:
-        assert SimResult.parse_obj(response.json()) == SimResult.from_sim(sim)
+        success_case(response_json, sim)
