ec9fc37e7fcc5a3cef6f0167f46c8e873d788f75
Jay Hilton <jhilton@mit.edu>
2021-07-25 19:00:38 -0400

    Mark tests as fuel_use_functions tests

--- spacenet/fuel_formulas/test/test_fuel_formulas.py
+++ spacenet/fuel_formulas/test/test_fuel_formulas.py
@@ -12,7 +12,7 @@ from .utilities import DeltaVTestCase, FinalMassTestCase
 from ..functions import delta_v_from, final_mass_from
 from .. import test
 
-pytestmark = [pytest.mark.unit]
+pytestmark = [pytest.mark.unit, pytest.mark.fuel_use_functions]
 
 POSITIVE_FINITE_FLOATS = st.floats(
     min_value=0, exclude_min=True, allow_nan=False, allow_infinity=False
@@ -88,11 +88,10 @@ def test_constant_mass_ratio_same_delta_v(
 )
 def test_same_delta_v_constant_mass_ratio(
     delta_v: float, isp: float, m_0: float, ratio: float
-):
+) -> None:
     assume(0 < m_0 * ratio < math.inf)
     m_f = final_mass_from(delta_v, isp, m_0)
     mass_ratio = m_f / m_0
-    # assume(0 < m_f < m_0)
     assert final_mass_from(delta_v, isp, m_0 * ratio) / (m_0 * ratio) == pytest.approx(
         mass_ratio, rel=10 ** -3
     )
@@ -120,7 +119,7 @@ def test_mf_to_dv_to_mf(isp: float, masses: Tuple[float, float]) -> None:
     isp=PRACTICAL_ISPS,
     m_0=POSITIVE_FINITE_FLOATS,
 )
-def test_dv_to_mf_to_dv(delta_v: float, isp: float, m_0: float):
+def test_dv_to_mf_to_dv(delta_v: float, isp: float, m_0: float) -> None:
     m_f = final_mass_from(delta_v, isp, m_0)
     assume(0 < m_f < m_0)
     dv_from_m_f = delta_v_from(isp, m_0, m_f)
