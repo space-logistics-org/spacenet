570ce0571b27db36fb56bf413af65d9e65a12406
Leigha Capra <13626046+leighacapra@users.noreply.github.com>
2021-07-26 15:01:27 -0400

    adjusted campaign main menu to accomodate visualizations

--- app/campaign/static/main_menu.js
+++ app/campaign/static/main_menu.js
@@ -4,6 +4,7 @@ function statusSet() {
   $("#resDirect").hide();
   $("#visDirect").hide();
   $("#netDirect").hide();
+  $("#visSpace").hide();
 }
 
 
@@ -55,61 +56,97 @@ function networkDiv(){
 
 
 function createDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="create_elements.html" ></object>';
 }
 
 function moveDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="move_elements.html" ></object>';
 }
 
 function removeDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="remove_elements.html" ></object>';
 }
 
 function consumeDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="consume_resources_formview.html" ></object>';
 }
 
 function transferDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="transfer_resources_formview.html" ></object>';
 }
 
 function batDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="bat_chart.html" ></object>';
 }
 
 function netDiv(){
-  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="network_visualization.html" ></object>';
+  $("#formSpace").hide();
+  $("#visSpace").show();
+  document.getElementById("visSpace").innerHTML='<object id = formSpace type="text/html" data="network_visualization.html" ></object>';
 }
 
 function crewDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="crewed_exploration_formview.html" ></object>';
 }
 
 function sptrDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="spaceTransport_formview.html" ></object>';
 }
 
 function netSelectDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="network_selection.html" ></object>';
 }
 
 function reconDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="reconfigure_element_formview.html" ></object>';
 }
 
 function fliDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="flight_transport_formview.html" ></object>';
 }
 
 function propBurnDiv(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="propulsive_burn_formview.html" ></object>';
 }
 
 function missDev(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="mission_demand_model.html" ></object>';
 }
 
 function crewEVA(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
   document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="crewedEVA_formview.html" ></object>';
+}
+
+function missDem(){
+  $("#formSpace").show();
+  $("#visSpace").hide();
+  document.getElementById("formSpace").innerHTML='<object id = formSpace type="text/html" data="mission_model_formview.html" ></object>';
 }
