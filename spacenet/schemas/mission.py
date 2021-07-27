e385905adf9bf5c3add35ac3e5280be32b2c5b5b
Jay Hilton <jhilton@mit.edu>
2021-07-26 22:43:35 -0400

    Use Events directly in Mission schema

--- spacenet/schemas/mission.py
+++ spacenet/schemas/mission.py
@@ -5,24 +5,50 @@ Created on Fri Jul  9 06:55:31 2021
 @author: jredm
 """
 from datetime import datetime
-from typing import List
+from typing import List, Union
+from uuid import UUID
 
 from pydantic import BaseModel, Field
 
+from . import Event
 from .mission_demand_model import MissionDemand
-from .types import SafeInt
+from .events import (
+    FlightTransport,
+    SpaceTransport,
+    SurfaceTransport,
+    MakeElements,
+    RemoveElements,
+    MoveElements,
+)
+
+EventTypes = Union[
+    FlightTransport,
+    SpaceTransport,
+    SurfaceTransport,
+    MoveElements,
+    RemoveElements,
+    MakeElements,
+]  # TODO: eventTypes should be expanded to include all the types of Event
 
 
 class Mission(BaseModel):  # TODO: should be UUIDs
     name: str = Field(..., title="Name", description="name of mission")
-    start_date: datetime = Field(..., title="Start Date", description="date of mission start")
-    events: List[SafeInt] = Field(..., title="Event ID List", description="list of event IDs")
-    demand_models: List[MissionDemand] = Field(..., title="Demand Models List",
-                                               description="list of mission demand models")
-    origin: SafeInt = Field(..., title="Origin ID", description="ID of origin node")
-    destination: SafeInt = Field(..., title="Destination ID",
-                                 description="ID of destination node")
-    return_origin: SafeInt = Field(..., title="Return Origin ID",
-                                   description="ID of return origin node")
-    return_destination: SafeInt = Field(..., title="Return Destination ID",
-                                        description="ID of return destination node")
+    start_date: datetime = Field(
+        ..., title="Start Date", description="date of mission start"
+    )
+    events: List[EventTypes] = Field(
+        ..., title="Event ID List", description="list of event IDs"
+    )
+    demand_models: List[MissionDemand] = Field(
+        ..., title="Demand Models List", description="list of mission demand models"
+    )
+    origin: UUID = Field(..., title="Origin ID", description="ID of origin node")
+    destination: UUID = Field(
+        ..., title="Destination ID", description="ID of destination node"
+    )
+    return_origin: UUID = Field(
+        ..., title="Return Origin ID", description="ID of return origin node"
+    )
+    return_destination: UUID = Field(
+        ..., title="Return Destination ID", description="ID of return destination node"
+    )
