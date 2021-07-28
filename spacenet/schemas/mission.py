7171a0061907d4f222b66d340fa1150894993246
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-07-28 10:07:50 -0400

    Update mission.py

--- spacenet/schemas/mission.py
+++ spacenet/schemas/mission.py
@@ -1,17 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 06:55:31 2021
+@author: jredm
+"""
 from datetime import datetime
-from typing import Any, List, Set
+from typing import List, Union
+from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+from . import Event
 from .mission_demand_model import MissionDemand
+from .events import (
+    FlightTransport,
+    SpaceTransport,
+    SurfaceTransport,
+    MakeElements,
+    RemoveElements,
+    MoveElements,
+)
+
+EventTypes = Union[
+    FlightTransport,
+    SpaceTransport,
+    SurfaceTransport,
+    MoveElements,
+    RemoveElements,
+    MakeElements,
+]  # TODO: eventTypes should be expanded to include all the types of Event
 
 
-class Mission(BaseModel):
-    name: str
-    start_date: datetime
-    events: List[Any]
-    demand_models: Set[MissionDemand]
-    origin: int
-    destination: int
-    return_origin: int
-    return_destination: int
+class Mission(BaseModel):  # TODO: should be UUIDs
+    name: str = Field(..., title="Name", description="name of mission")
+    start_date: datetime = Field(
+        ..., title="Start Date", description="date of mission start"
+    )
+    events: List[EventTypes] = Field(
+        ..., title="Event ID List", description="list of event IDs"
+    )
+    demand_models: List[MissionDemand] = Field(
+        ..., title="Demand Models List", description="list of mission demand models"
+    )
+    origin: UUID = Field(..., title="Origin ID", description="ID of origin node")
+    destination: UUID = Field(
+        ..., title="Destination ID", description="ID of destination node"
+    )
+    return_origin: UUID = Field(
+        ..., title="Return Origin ID", description="ID of return origin node"
+    )
+    return_destination: UUID = Field(
+        ..., title="Return Destination ID", description="ID of return destination node"
+    )
