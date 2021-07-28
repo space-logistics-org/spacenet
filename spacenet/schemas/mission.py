dc1a521e7bcdd173dec173365091ebf8b8ec8f7d
Jay Hilton <jhilton@mit.edu>
2021-07-28 15:10:48 -0400

    Add CrewedExploration schema to Mission events

--- spacenet/schemas/mission.py
+++ spacenet/schemas/mission.py
@@ -10,7 +10,6 @@ from uuid import UUID
 
 from pydantic import BaseModel, Field
 
-from . import Event
 from .mission_demand_model import MissionDemand
 from .events import (
     FlightTransport,
@@ -19,16 +18,18 @@ from .events import (
     MakeElements,
     RemoveElements,
     MoveElements,
+    CrewedExploration
 )
 
-EventTypes = Union[
+AllEvents = Union[
     FlightTransport,
     SpaceTransport,
     SurfaceTransport,
     MoveElements,
     RemoveElements,
     MakeElements,
-]  # TODO: eventTypes should be expanded to include all the types of Event
+    CrewedExploration
+]  # TODO: AllEvents should be expanded to include all the types of Event
 
 
 class Mission(BaseModel):  # TODO: should be UUIDs
@@ -36,7 +37,7 @@ class Mission(BaseModel):  # TODO: should be UUIDs
     start_date: datetime = Field(
         ..., title="Start Date", description="date of mission start"
     )
-    events: List[EventTypes] = Field(
+    events: List[AllEvents] = Field(
         ..., title="Event ID List", description="list of event IDs"
     )
     demand_models: List[MissionDemand] = Field(
