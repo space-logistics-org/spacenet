1c696a742208f83db968972f70d701b60f02673f
Jay Hilton <jhilton@mit.edu>
2021-07-28 15:11:41 -0400

    Remove xfail marker for Apollo 17 parsing test

--- spacenet/schemas/test/test_with_apollo_17_data.py
+++ spacenet/schemas/test/test_with_apollo_17_data.py
@@ -9,7 +9,6 @@ from .. import Scenario
 pytestmark = [pytest.mark.schema, pytest.mark.apollo_17]
 
 
-@pytest.mark.xfail(reason="apollo 17 data doesn't match schema at present")
 def test_scenario_parses():
     filename = pkg_resources.resource_filename("spacenet.schemas", "apollo_17/apollo_17.json")
     parse_file_as(Scenario, filename)
