cff93a77a9bce45cd2d75da4d59c01c137e9a134
Jay Hilton <jhilton@mit.edu>
2021-07-30 08:17:16 -0500

    Change event tests to check for type

--- spacenet/schemas/test/events/test_move_element_event.py
+++ spacenet/schemas/test/events/test_move_element_event.py
@@ -2,14 +2,16 @@ import pytest
 from hypothesis import given, strategies as st
 
 from spacenet.schemas.element_events import MoveElements
-from .utilities import EVENT_INVALID_MAP, EVENT_VALID_MAP, VALID_PRIORITIES, INVALID_PRIORITIES
+from .utilities import EVENT_INVALID_MAP, EVENT_VALID_MAP
 from ..utilities import (
     INVALID_UUIDS,
     success_from_kw,
-    xfail_from_kw,
+    valid_invalid_from_allowed, xfail_from_kw,
 )
 
 pytestmark = [pytest.mark.unit, pytest.mark.event, pytest.mark.schema]
+
+VALID_TYPES, INVALID_TYPES = valid_invalid_from_allowed(["MoveElements"])
 
 VALID_MAP = {
     "to_move": st.lists(
@@ -17,25 +19,28 @@ VALID_MAP = {
     ),
     "origin_id": st.uuids(),
     "destination_id": st.uuids(),
-    **EVENT_VALID_MAP
+    **EVENT_VALID_MAP,
+    "type": VALID_TYPES
 }
 
 INVALID_MAP = {
     "to_move": st.lists(INVALID_UUIDS, min_size=1),
     "origin_id": INVALID_UUIDS,
     "destination_id": INVALID_UUIDS,
-    **EVENT_INVALID_MAP
+    **EVENT_INVALID_MAP,
+    "type": INVALID_TYPES,
 }
 
 
-def xfail_construct_move(to_move, origin_id, destination_id, priority, mission_time):
+def xfail_construct_move(to_move, origin_id, destination_id, priority, mission_time, type):
     xfail_from_kw(
         MoveElements,
         to_move=to_move,
         origin_id=origin_id,
         destination_id=destination_id,
         priority=priority,
-        mission_time=mission_time
+        mission_time=mission_time,
+        type=type
     )
 
 
@@ -70,10 +75,20 @@ def test_invalid_origin_id(kw):
 def test_invalid_destination_id(kw):
     xfail_construct_move(**kw)
 
+
 @given(
     kw=st.fixed_dictionaries(
         mapping={**VALID_MAP, "priority": INVALID_MAP["priority"]}
     )
 )
 def test_invalid_priority(kw):
+    xfail_construct_move(**kw)
+
+
+@given(
+    kw=st.fixed_dictionaries(
+        mapping={**VALID_MAP, "type": INVALID_MAP["type"]}
+    )
+)
+def test_invalid_type(kw):
     xfail_construct_move(**kw)
--- spacenet/schemas/test/events/test_remove_element_event.py
+++ spacenet/schemas/test/events/test_remove_element_event.py
@@ -14,25 +14,28 @@ pytestmark = [pytest.mark.unit, pytest.mark.event, pytest.mark.schema]
 
 ALLOWED_REMOVAL_POINTS = ["Node", "Edge"]
 VALID_REMOVALS, INVALID_REMOVALS = valid_invalid_from_allowed(ALLOWED_REMOVAL_POINTS)
+VALID_TYPES, INVALID_TYPES = valid_invalid_from_allowed(["RemoveElements"])
 
 VALID_MAP = {
     "elements": st.lists(
         st.uuids()
     ),
     "removal_point_id": st.uuids(),
-    **EVENT_VALID_MAP
+    **EVENT_VALID_MAP,
+    "type": VALID_TYPES,
 }
 
 INVALID_MAP = {
     "elements": st.lists(INVALID_UUIDS, min_size=1),
     "removal_point_id": INVALID_UUIDS,
-    **EVENT_INVALID_MAP
+    **EVENT_INVALID_MAP,
+    "type": INVALID_TYPES
 }
 
 
-def xfail_construct_remove(elements, removal_point_id, priority, mission_time):
+def xfail_construct_remove(elements, removal_point_id, priority, mission_time, type):
     xfail_from_kw(
-        RemoveElements, elements=elements, removal_point_id=removal_point_id, priority=priority, mission_time=mission_time
+        RemoveElements, elements=elements, removal_point_id=removal_point_id, priority=priority, mission_time=mission_time, type=type
     )
 
 
@@ -68,4 +71,13 @@ def test_invalid_removal_point_id(kw):
     )
 )
 def test_invalid_priority(kw):
+    xfail_construct_remove(**kw)
+
+
+@given(
+    kw=st.fixed_dictionaries(
+        mapping={**VALID_MAP, "type": INVALID_MAP["type"]}
+    )
+)
+def test_invalid_type(kw):
     xfail_construct_remove(**kw)
