feebfd1e6114a0827efddd61665eb3549485b1a7
Jay Hilton <jhilton@mit.edu>
2021-07-30 08:23:32 -0500

    Change element event schemas to provide type

--- spacenet/schemas/test/events/test_reconfigure_element_event.py
+++ spacenet/schemas/test/events/test_reconfigure_element_event.py
@@ -11,48 +11,36 @@ from ..utilities import (
 
 pytestmark = [pytest.mark.unit, pytest.mark.event, pytest.mark.schema]
 
-VALID_TO_RECONFIGURE = st.dictionaries(
-    keys=st.uuids(),
-    values=st.uuids(),
-)
+VALID_TO_RECONFIGURE = st.dictionaries(keys=st.uuids(), values=st.uuids(),)
 INVALID_TO_RECONFIGURE = st.one_of(
-    st.dictionaries(
-        keys=INVALID_UUIDS,
-        values=st.uuids(),
-        min_size=1,
-    ),
-    st.dictionaries(
-        keys=st.integers(),
-        values=INVALID_UUIDS,
-        min_size=1,
-    ),
-    st.dictionaries(
-        keys=INVALID_UUIDS,
-        values=INVALID_UUIDS,
-        min_size=1,
-    ),
+    st.dictionaries(keys=INVALID_UUIDS, values=st.uuids(), min_size=1,),
+    st.dictionaries(keys=st.integers(), values=INVALID_UUIDS, min_size=1,),
+    st.dictionaries(keys=INVALID_UUIDS, values=INVALID_UUIDS, min_size=1,),
 )
 
 VALID_MAP = {
     "to_reconfigure": VALID_TO_RECONFIGURE,
     "reconfigure_point_id": st.uuids(),
-    **EVENT_VALID_MAP
+    **EVENT_VALID_MAP,
 }
 
 INVALID_MAP = {
     "to_reconfigure": INVALID_TO_RECONFIGURE,
     "reconfigure_point_id": INVALID_UUIDS,
-    "priority": INVALID_PRIORITIES
+    "priority": INVALID_PRIORITIES,
 }
 
 
-def xfail_construct_reconfigure(to_reconfigure, reconfigure_point_id, priority, mission_time):
+def xfail_construct_reconfigure(
+    to_reconfigure, reconfigure_point_id, priority, mission_time, type
+):
     xfail_from_kw(
         ReconfigureElements,
         to_reconfigure=to_reconfigure,
         reconfigure_point_id=reconfigure_point_id,
         priority=priority,
-        mission_time=mission_time
+        mission_time=mission_time,
+        type=type,
     )
 
 
@@ -84,10 +72,7 @@ def test_invalid_reconfigure_point_id(kw):
 
 @given(
     kw=st.fixed_dictionaries(
-        mapping={
-            **VALID_MAP,
-            "priority": INVALID_MAP["priority"],
-        }
+        mapping={**VALID_MAP, "priority": INVALID_MAP["priority"],}
     )
 )
 def test_invalid_priority(kw):
