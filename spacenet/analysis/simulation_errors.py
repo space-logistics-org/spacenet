54c9cc39fc7edef330fccb53b74be65cce30f605
aleph-oh <jhilton@mit.edu>
2021-08-03 15:59:20 -0400

    Change simulation errors to have distinct types

--- spacenet/analysis/simulation_errors.py
+++ spacenet/analysis/simulation_errors.py
@@ -9,21 +9,51 @@ class SimError(BaseModel):
     description: str
 
     @staticmethod
-    def does_not_exist(timestamp: datetime, id_: UUID) -> "SimError":
-        return SimError(
+    def does_not_exist(timestamp: datetime, id_: UUID) -> "EntityDoesNotExist":
+        return EntityDoesNotExist.new(timestamp, id_)
+
+    @staticmethod
+    def not_an_element(timestamp: datetime, id_: UUID) -> "EntityNotAnElement":
+        return EntityNotAnElement.new(timestamp, id_)
+
+    @staticmethod
+    def not_a_container(timestamp: datetime, id_: UUID) -> "EntityNotAContainer":
+        return EntityNotAContainer.new(timestamp, id_)
+
+    @staticmethod
+    def not_at_location(
+        timestamp: datetime, id_: UUID, location: UUID
+    ) -> "EntityNotAtLocation":
+        return EntityNotAtLocation.new(timestamp, id_, location)
+
+
+class EntityDoesNotExist(SimError):
+    @staticmethod
+    def new(timestamp: datetime, id_: UUID) -> "EntityDoesNotExist":
+        return EntityDoesNotExist(
             timestamp=timestamp, description=f"No entity with id {id_} in namespace"
         )
 
-    @staticmethod
-    def not_an_element(timestamp: datetime, id_: UUID) -> "SimError":
-        return SimError(timestamp=timestamp, description=f"ID {id_} is not an element")
 
+class EntityNotAnElement(SimError):
     @staticmethod
-    def not_a_container(timestamp: datetime, id_: UUID) -> "SimError":
-        return SimError(timestamp=timestamp, description=f"ID {id_} is not a container")
+    def new(timestamp: datetime, id_: UUID) -> "EntityNotAnElement":
+        return EntityNotAnElement(
+            timestamp=timestamp, description=f"ID {id_} is not an element"
+        )
 
+
+class EntityNotAContainer(SimError):
     @staticmethod
-    def not_at_location(timestamp: datetime, id_: UUID, location: UUID) -> "SimError":
-        return SimError(
+    def new(timestamp: datetime, id_: UUID) -> "EntityNotAContainer":
+        return EntityNotAContainer(
+            timestamp=timestamp, description=f"ID {id_} is not a container"
+        )
+
+
+class EntityNotAtLocation(SimError):
+    @staticmethod
+    def new(timestamp: datetime, id_: UUID, location: UUID) -> "EntityNotAtLocation":
+        return EntityNotAtLocation(
             timestamp=timestamp, description=f"ID {id_} is not at location {location}"
         )
