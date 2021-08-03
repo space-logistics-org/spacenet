772813a13bce955431cd7156b92f4ff8531babbf
Jay Hilton <jhilton@mit.edu>
2021-08-03 13:46:21 -0400

    Use parse_obj to compare result in simulation test

--- app/campaign/api/test/test_spatial_simulation.py
+++ app/campaign/api/test/test_spatial_simulation.py
@@ -18,14 +18,19 @@ client = TestClient(app)
 @given(scenario=st.builds(Scenario))
 def test_only_allowed_status_codes(scenario: Scenario):
     response = client.post("/simulation/", json=jsonable_encoder(scenario.dict()))
-    assert 200 == response.status_code
-    # for now 200 is the only acceptable status code;
-    # may allow different status code for error case
+    if response.status_code == 422:  # TODO: change this when 422 is no longer returned
+        try:
+            Simulation(scenario)
+        except ValueError:
+            return
+        else:
+            assert False
+    assert response.status_code == 200
 
 
 @pytest.mark.slow
 @pytest.mark.xfail
-@given(scenario=st.builds(Scenario))
+@given(scenario=st.builds(Scenario))  # TODO: need an improved scenario builder
 def test_same_result_as_analysis(scenario: Scenario):
     response = client.post("/simulation/", json=jsonable_encoder(scenario.dict()))
     response_json = response.json()
@@ -35,4 +40,6 @@ def test_same_result_as_analysis(scenario: Scenario):
         assume(False)
         return
     sim.run()
-    assert ResultAndErrors(result=sim.result(), errors=sim.errors) == response_json
+    assert ResultAndErrors(
+        result=sim.result(), errors=sim.errors
+    ) == ResultAndErrors.parse_obj(response_json)
