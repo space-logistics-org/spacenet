8c747bdc7b1c9c4753bb6baaca365b3fb843db7b
aleph-oh <jhilton@mit.edu>
2021-08-10 14:21:19 -0400

    Document exceptions module

--- spacenet/analysis/exceptions.py
+++ spacenet/analysis/exceptions.py
@@ -8,20 +8,38 @@ __all__ = [
 
 
 class SimException(Exception):
+    """
+    Base class for exceptions raised during simulation.
+    """
     pass
 
 
 class EventDateOverflowError(SimException, OverflowError):
+    """
+    Exception representing the dates in an event being too large to be represented in a
+    datetime object.
+    """
     pass
 
 
 class UnrecognizedEdgeEndpoint(SimException, ValueError):
+    """
+    Exception representing that an edge has an endpoint which is not a node in the network.
+    """
     pass
 
 
 class UnrecognizedID(SimException, ValueError):
+    """
+    Exception representing that an ID was found which does not refer to an entity in
+    simulation.
+    """
     pass
 
 
 class MismatchedIDType(SimException, TypeError):
+    """
+    Exception representing that the type of the entity being referenced by the ID is not
+    suitable as it's being used, although the UUID does refer to an entity in the simulation.
+    """
     pass
