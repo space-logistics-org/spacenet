e170225b4f075d291b22428fb4a218246a4f68ae
TheoSherman29 <sherman.theodore@gmail.com>
2021-08-13 15:38:11 -0400

    Add Mars Exploration scenario and tests

---
+++ spacenet/analysis/test/test_with_mars_exploration_data.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import pkg_resources
+import pytest
+from hypothesis import assume, given, strategies as st
+
+from spacenet.analysis.checked_scenario import CheckedScenario
+from spacenet.analysis.simulation import Simulation
+
+
+pytestmark = [pytest.mark.analysis, pytest.mark.mars_exploration]
+
+
+def test_scenario_runs_without_error():
+    filename = pkg_resources.resource_filename(
+        "spacenet.schemas", "other_scenarios/mars_exploration.json"
+    )
+    scenario = CheckedScenario.parse_file(filename)
+    sim = Simulation(scenario)
+    sim.run()
+    assert not sim.errors, "expected no errors"
+
+
+@given(st.datetimes())
+def test_scenario_runs_without_error_for_any_until(dt: datetime):
+    filename = pkg_resources.resource_filename(
+        "spacenet.schemas", "other_scenarios/mars_exploration.json"
+    )
+    scenario = CheckedScenario.parse_file(filename)
+    sim = Simulation(scenario)
+    sim.run(until=dt)
+    assert not sim.errors, "expected no errors"
+
+
+@given(st.datetimes())
+def test_identical_result_for_datetime_after_last_event(dt: datetime):
+    filename = pkg_resources.resource_filename(
+        "spacenet.schemas", "other_scenarios/mars_exploration.json"
+    )
+    scenario = CheckedScenario.parse_file(filename)
+    sim = Simulation(scenario)
+    sim.run()
+    assume(dt >= sim.current_time)
+    stopping_sim = Simulation(scenario)
+    stopping_sim.run(until=dt)
+    assert sim.result() == stopping_sim.result()
