f88103a932dc04bf80f342ee08e033e30c863b26
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-08-18 12:38:02 -0400

    Update element.py

--- app/database/api/routers/element.py
+++ app/database/api/routers/element.py
@@ -1,3 +1,7 @@
+"""
+This module specifies a CRUD route for the element schema.
+"""
+
 from .base_router import CRUDRouter
 from ..models import element as models
 from ..schemas.constants import ELEMENT_SCHEMAS
