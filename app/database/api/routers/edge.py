804c111cc0c81380e72e9145c289db135052f88e
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-08-18 12:35:47 -0400

    Add docstring to edge.py

--- app/database/api/routers/edge.py
+++ app/database/api/routers/edge.py
@@ -1,3 +1,7 @@
+"""
+This module specifies a CRUD route for the edge schema.
+"""
+
 from .base_router import CRUDRouter
 from ..models import edge as models
 from ..schemas.constants import EDGE_SCHEMAS
