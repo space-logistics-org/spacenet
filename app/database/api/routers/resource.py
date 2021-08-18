7d4d2e0ab0992327fcd6913f20466ba31dc364ca
walpat211 <43225908+walpat211@users.noreply.github.com>
2021-08-18 12:39:15 -0400

    Update resource.py

--- app/database/api/routers/resource.py
+++ app/database/api/routers/resource.py
@@ -1,3 +1,7 @@
+"""
+This module specifies a CRUD route for the resource schema.
+"""
+
 from .base_router import CRUDRouter
 from ..models import resource as models
 from ..schemas.constants import RESOURCE_SCHEMAS
