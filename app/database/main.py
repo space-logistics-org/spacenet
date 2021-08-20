4ab7d313e33dfadf4837936781b7fce43c658d6a
aleph-oh <jhilton@mit.edu>
2021-08-20 01:06:34 -0400

    Document modules in app/database

--- app/database/main.py
+++ app/database/main.py
@@ -1,3 +1,6 @@
+"""
+This module includes application routers and static files used for the database editor.
+"""
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
