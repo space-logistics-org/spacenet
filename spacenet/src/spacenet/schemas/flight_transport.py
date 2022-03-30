a48b9b7082999c37c081fb34f2ad6e92485a6131
Paul Grogan <pgrogan@stevens.edu>
2022-03-29 23:23:37 -0400

    revise folder structure to allow pyproject.toml build/installation

---
+++ spacenet/src/spacenet/schemas/flight_transport.py
@@ -0,0 +1,29 @@
+"""
+This module defines a schema for specifying flight transport events, representing cases where
+a vehicle is known to be able to traverse the given edge.
+"""
+from typing import List
+from typing_extensions import Literal
+from uuid import UUID
+
+from pydantic import Field
+
+from spacenet.schemas import ElementTransportEvent
+
+
+__all__ = ["FlightTransport"]
+
+
+class FlightTransport(ElementTransportEvent):
+    """
+    Schema for Flight Transport
+    """
+
+    name: str = Field(..., title="Name", description="The flight transport name")
+
+    elements_id_list: List[UUID] = Field(
+        ...,
+        title="List of Element IDs",
+        description="The list of IDs of elements being transported",
+    )
+    type: Literal["FlightTransport"]
---
+++ spacenet/src/spacenet/schemas/transfer_resources.py
@@ -0,0 +1,33 @@
+"""
+This module defines schemas for transferring resources between locations.
+"""
+from typing import List
+from uuid import UUID
+
+from pydantic import Field
+
+__all__ = ["TransferResourcesEvent"]
+
+from spacenet.schemas import Event
+
+
+class TransferResourcesEvent(Event):
+    """
+    An event that moves resources at a specific time and location (node or edge)
+    from an origin resource container to a destination resource container
+    """
+
+    to_transfer: List[UUID] = Field(
+        ..., description="the list of resource IDs to transfer"
+    )
+
+    origin_id: UUID = Field(
+        ...,
+        description="the ID of the original time and location which the "
+        "resources are being transferred from",
+    )
+
+    destination_id: UUID = Field(
+        ...,
+        description="the ID of the new location which the elements are being transferred to",
+    )
