60078418ae8618c40e10521d56f886907852ea89
Sarah Bentley <sarahbentley@dhcp-10-31-84-160.dyn.MIT.EDU>
2022-04-28 08:29:16 -0400

    changing names to snake case

--- spacenet/src/spacenet/schemas/element_demand_model.py
+++ spacenet/src/spacenet/schemas/element_demand_model.py
@@ -45,61 +45,61 @@ class CrewConsumablesDemandModel(DemandModel):
     Demands for consumables by crew.
 
     :param CrewConsumables type: 
-    :param float reservesDuration: 
-    :param float waterRecoveryRate: 
-    :param float clothingLifetime: 
-    :param float transitDemandOmitted: 
-    :param float waterRate: 
-    :param float evaWaterRate: 
-    :param float foodSupportRate: 
-    :param float ambientFoodRate: 
-    :param float rfFoodRate: 
-    :param float oxygenRate: 
-    :param float evaOxygenRate: 
-    :param float nitrogenRate: 
-    :param float hygieneRate: 
-    :param float hygieneKit: 
-    :param float clothingRate: 
-    :param float personalItems: 
-    :param float officeEquipment: 
-    :param float evaSuit: 
-    :param float evaLithiumHydroxide: 
-    :param float healthEquipment: 
-    :param float healthConsumables: 
-    :param float safetyEquipment: 
-    :param float commEquipment: 
-    :param float computerEquipment: 
-    :param float trashBagRate: 
-    :param float wasteContainmentRate: 
+    :param float reserves_duration: 
+    :param float water_recovery_rate: 
+    :param float clothing_lifetime: 
+    :param float transit_demand_omitted: 
+    :param float water_rate: 
+    :param float eva_water_rate: 
+    :param float food_support_rate: 
+    :param float ambient_food_rate: 
+    :param float rf_food_rate: 
+    :param float oxygen_rate: 
+    :param float eva_oxygen_rate: 
+    :param float nitrogen_rate: 
+    :param float hygiene_rate: 
+    :param float hygiene_kit: 
+    :param float clothing_rate: 
+    :param float personal_items: 
+    :param float office_equipment: 
+    :param float eva_suit: 
+    :param float eva_lithium_hydroxide: 
+    :param float health_equipment: 
+    :param float health_consumables: 
+    :param float safety_equipment: 
+    :param float comm_equipment: 
+    :param float computer_equipment: 
+    :param float trash_bag_rate: 
+    :param float waste_containment_rate: 
     """
     type: Literal[DemandModelKind.CrewConsumables] = Field(DemandModelKind.CrewConsumables, description="the demand model's type")
 
-    reservesDuration: float = Field(..., title="Reserves Duration")
-    waterRecoveryRate: float = Field(..., title="Water Recovery Rate")
-    clothingLifetime: float = Field(..., title="Clothing Lifetime")
-    transitDemandOmitted: bool = Field(..., title="Transit Demand Omitted")
-    waterRate: float = Field(default=3.6, title="Water Rate")
-    evaWaterRate: float = Field(default=0.6875, title="EVA Water Rate")
-    foodSupportRate: float = Field(default=0.05556, title="Food Support Rate")
-    ambientFoodRate: float = Field(default=0.76389, title="Ambient Food Rate")
-    rfFoodRate: float = Field(default=1.61667, title="Rf Food Rate")
-    oxygenRate: float = Field(default=3.85714, title="Oxygen Rate")
-    evaOxygenRate: float = Field(default=0.07875, title="EVA Oxygen Rate")
-    nitrogenRate: float = Field(default=2.21429, title="Nitrogen Rate")
-    hygieneRate: float = Field(default=0.27778, title="Hygiene Rate")
-    hygieneKit: float = Field(default=1.8, title="Hygiene Kit")
-    clothingRate: float = Field(default=2.3, title="Clothing Rate")
-    personalItems: float = Field(default=10, title="Personal Items")
-    officeEquipment: float = Field(default=5, title="Office Equipment")
-    evaSuit: float = Field(default=107, title="EVA Suit")
-    evaLithiumHydroxide: float = Field(default=0.3625, title="EVA Lithium Hydroxide")
-    healthEquipment: float = Field(default=20, title="Health Equipment")
-    healthConsumables: float = Field(default=0.1, title="Health Consumables")
-    safetyEquipment: float = Field(default=25, title="Safety Equipment")
-    commEquipment: float = Field(default=20, title="Comm Equipment")
-    computerEquipment: float = Field(default=5, title="Computer Equipment")
-    trashBagRate: float = Field(default=0.05, title="Trash Bag Rate")
-    wasteContainmentRate: float = Field(default=0.05, title="Waste Containment Rate")
+    reserves_duration: float = Field(..., title="Reserves Duration")
+    water_recovery_rate: float = Field(..., title="Water Recovery Rate")
+    clothing_lifetime: float = Field(..., title="Clothing Lifetime")
+    transit_demand_omitted: bool = Field(..., title="Transit Demand Omitted")
+    water_rate: float = Field(default=3.6, title="Water Rate")
+    eva_water_rate: float = Field(default=0.6875, title="EVA Water Rate")
+    food_support_rate: float = Field(default=0.05556, title="Food Support Rate")
+    ambient_food_rate: float = Field(default=0.76389, title="Ambient Food Rate")
+    rf_food_rate: float = Field(default=1.61667, title="Rf Food Rate")
+    oxygen_rate: float = Field(default=3.85714, title="Oxygen Rate")
+    eva_oxygen_rate: float = Field(default=0.07875, title="EVA Oxygen Rate")
+    nitrogen_rate: float = Field(default=2.21429, title="Nitrogen Rate")
+    hygiene_rate: float = Field(default=0.27778, title="Hygiene Rate")
+    hygiene_kit: float = Field(default=1.8, title="Hygiene Kit")
+    clothing_rate: float = Field(default=2.3, title="Clothing Rate")
+    personal_items: float = Field(default=10, title="Personal Items")
+    office_equipment: float = Field(default=5, title="Office Equipment")
+    eva_suit: float = Field(default=107, title="EVA Suit")
+    eva_lithium_hydroxide: float = Field(default=0.3625, title="EVA Lithium Hydroxide")
+    health_equipment: float = Field(default=20, title="Health Equipment")
+    health_consumables: float = Field(default=0.1, title="Health Consumables")
+    safety_equipment: float = Field(default=25, title="Safety Equipment")
+    comm_equipment: float = Field(default=20, title="Comm Equipment")
+    computer_equipment: float = Field(default=5, title="Computer Equipment")
+    trash_bag_rate: float = Field(default=0.05, title="Trash Bag Rate")
+    waste_containment_rate: float = Field(default=0.05, title="Waste Containment Rate")
 
 
 class TimedImpulseDemandModel(DemandModel):
@@ -131,10 +131,9 @@ class SparingByMassDemandModel(DemandModel):
     Demand model consuming a resource in one instant.
 
     :param SparingByMass type: demand model's type
-    :param [Demand] demands: a list of the rated demands of the given demand model
-    :param float unpressurizedSparesRate
-    :param float pressurizedSparesRate
-    :param float partsListEnabled
+    :param float unpressurizedSparesRate: percentage of an element's mass demanded as unpressurized spares each year
+    :param float pressurizedSparesRate: percentage of an element's mass demanded as pressurized spares each year
+    :param bool partsListEnabled: true if the element's part list should be used to drive demand resources
 
     """
     type: Literal[DemandModelKind.SparingByMass] = Field(DemandModelKind.SparingByMass, description="the demand model's type")
