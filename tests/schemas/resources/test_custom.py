5ac0c13497be3d53010175d68c67e8844ff5ec55
Paul Grogan <pgrogan@stevens.edu>
2023-03-04 18:56:08 -0500

    add tests for resources

---
+++ tests/schemas/resources/test_custom.py
@@ -0,0 +1,30 @@
+import unittest
+from uuid import uuid4
+
+from spacenet.schemas import ClassOfSupply, Environment, ResourceAmount, ResourceAmountRate
+
+
+class TestResourceAmount(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {
+            "resource": uuid4(),
+            "amount": 10.5
+        }
+        self.test_amount = ResourceAmount(**self.test_data)
+
+    def test_good_data(self):
+        self.assertEqual(self.test_amount.resource, self.test_data.get("resource"))
+        self.assertEqual(self.test_amount.amount, self.test_data.get("amount"))
+
+
+class TestResourceAmountRate(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {
+            "resource": uuid4(),
+            "rate": 10.5
+        }
+        self.test_rate = ResourceAmountRate(**self.test_data)
+
+    def test_good_data(self):
+        self.assertEqual(self.test_rate.resource, self.test_data.get("resource"))
+        self.assertEqual(self.test_rate.rate, self.test_data.get("rate"))
---
+++ tests/schemas/resources/test_resource.py
@@ -0,0 +1,60 @@
+import unittest
+
+from spacenet.schemas import ClassOfSupply, ContinuousResource, DiscreteResource, Environment, ResourceType
+
+class TestResourceType(unittest.TestCase):
+    def test_discrete_serialization(self):
+        self.assertEqual(ResourceType.DISCRETE, "Discrete")
+    
+    def test_continuous_serialization(self):
+        self.assertEqual(ResourceType.CONTINUOUS, "Continuous")
+        
+class TestDiscreteResource(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {
+            "name": "test resource",
+            "class_of_supply": ClassOfSupply.COS_2,
+            "environment": Environment.PRESSURIZED,
+            "packing_factor": 0.5,
+            "units": "kg",
+            "description": "example description",
+            "unit_mass": 1.0,
+            "unit_volume": 0.1
+        }
+        self.test_resource = DiscreteResource(**self.test_data)
+
+    def test_good_data(self):
+        self.assertEqual(self.test_resource.type, ResourceType.DISCRETE)
+        self.assertEqual(self.test_resource.name, self.test_data.get("name"))
+        self.assertEqual(self.test_resource.class_of_supply, self.test_data.get("class_of_supply"))
+        self.assertEqual(self.test_resource.environment, self.test_data.get("environment"))
+        self.assertEqual(self.test_resource.packing_factor, self.test_data.get("packing_factor"))
+        self.assertEqual(self.test_resource.units, self.test_data.get("units"))
+        self.assertEqual(self.test_resource.description, self.test_data.get("description"))
+        self.assertEqual(self.test_resource.unit_mass, self.test_data.get("unit_mass"))
+        self.assertEqual(self.test_resource.unit_volume, self.test_data.get("unit_volume"))
+
+class TestContinuousResource(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {
+            "name": "test resource",
+            "class_of_supply": ClassOfSupply.COS_2,
+            "environment": Environment.PRESSURIZED,
+            "packing_factor": 0.5,
+            "units": "kg",
+            "description": "example description",
+            "unit_mass": 1.0,
+            "unit_volume": 0.1
+        }
+        self.test_resource = ContinuousResource(**self.test_data)
+
+    def test_good_data(self):
+        self.assertEqual(self.test_resource.type, ResourceType.CONTINUOUS)
+        self.assertEqual(self.test_resource.name, self.test_data.get("name"))
+        self.assertEqual(self.test_resource.class_of_supply, self.test_data.get("class_of_supply"))
+        self.assertEqual(self.test_resource.environment, self.test_data.get("environment"))
+        self.assertEqual(self.test_resource.packing_factor, self.test_data.get("packing_factor"))
+        self.assertEqual(self.test_resource.units, self.test_data.get("units"))
+        self.assertEqual(self.test_resource.description, self.test_data.get("description"))
+        self.assertEqual(self.test_resource.unit_mass, self.test_data.get("unit_mass"))
+        self.assertEqual(self.test_resource.unit_volume, self.test_data.get("unit_volume"))
